57a2684b8b302e2e9f6a1acff355a713bd2d5976
moneshkannan <moneshkannan02@gmail.com>
2022-02-27 14:04:38 +0530

    fixed navigation

--- App.js
+++ App.js
@@ -4,14 +4,37 @@ import {Provider} from 'react-redux'
 import HomeScreen from './screens/HomeScreen';
 import { store } from './store';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
+// import 'react-native-gesture-handler'
+import { NavigationContainer } from '@react-navigation/native';
+// import { createStackNavigator } from '@react-navigation/stack';
+import {createNativeStackNavigator} from '@react-navigation/native-stack'
+import MapScreen from './screens/MapScreen';
 
 export default function App() {
+  const Stack = createNativeStackNavigator();
   return (
     <Provider store={store}>
-      <SafeAreaProvider>
-        <StatusBar style="auto" />
-        <HomeScreen/>
-      </SafeAreaProvider>
+      <NavigationContainer>
+        <SafeAreaProvider>
+          <StatusBar style="auto" />
+          <Stack.Navigator>
+            <Stack.Screen
+              name='HomeScreen'
+              component={HomeScreen}
+              options={{
+                headerShown: false
+              }}
+            />
+            <Stack.Screen
+              name='MapScreen'
+              component={MapScreen}
+              options={{
+                headerShown: false
+              }}
+            />
+          </Stack.Navigator>
+        </SafeAreaProvider>
+      </NavigationContainer>
     </Provider>
   );
 }
--- components/NavOptions.js
+++ components/NavOptions.js
@@ -2,6 +2,7 @@ import { FlatList, Image, StyleSheet, Text, TouchableOpacity, View } from 'react
 import React from 'react'
 import tw from 'tailwind-react-native-classnames'
 import { Icon } from 'react-native-elements';
+import { useNavigation } from '@react-navigation/native';
 
 
 const data =[
@@ -19,6 +20,7 @@ const data =[
     }
 ];
 const NavOptions = () => {
+    const navigation = useNavigation();
   return (
     <FlatList
         data={data}
@@ -26,6 +28,7 @@ const NavOptions = () => {
         keyExtractor={(item) => item.id}
         renderItem={({item})=>(
             <TouchableOpacity
+                onPress={()=>navigation.navigate(item.screen)}
                 style={tw`p-2 pl-6 pb-8 pt-4 bg-gray-200 m-2 w-40`}
             >
                 <Image
